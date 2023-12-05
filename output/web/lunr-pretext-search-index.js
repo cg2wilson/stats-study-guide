ccf1e49f52af3c11d06d052a34a0845e6dfd5056
cory <cory.g.wilson@gmail.com>
2023-12-05 12:07:25 -0600

    started week 1

--- output/web/lunr-pretext-search-index.js
+++ output/web/lunr-pretext-search-index.js
@@ -16,7 +16,7 @@ var ptx_lunr_docs = [
   "type": "Section",
   "number": "1.1",
   "title": "Statistics Terminology",
-  "body": " Statistics Terminology   This section corresponds to Section 1.2 in Dietz    Population  Population   A population is a collection of individuals (not necessarily people) that share one or more common characteristics. In statistics this is the entire group of individuals for which information is being gathered or studied. Information that describes the population is called descriptive statistics.     Individual  Individual   An individual is a member of a particular population     Sample  Sample   A sample is a subset of the population. In statistics, it is often not feasible or desirable to gather information about the whole population so information is gathered on part of the population being studied. Inferential Statistics is when the information gathered about the sample is used to say something about the entire population.     Variable  Variable   A variable is a quantity or quality of the population that varies among individuals. A variable can be either quantitative or qualitative (categorical). A quantitative variable can be measured and can often be used in the calculation of an average. Quantitative variables have numerical values. Categorical variables are often a verbal description of the population but they can sometimes be numerical if they are used to categorize individuals in the population. Examples of this are zip codes, phone numbers, and instances when numbers are substituted for a particular quality such as letting 0 represent Males and 1 Females.     Parameter  Parameter   A parameter is a value found by looking at the entire population. Information you see about small populations is often parameters because it is not too hard to gather information about each individual in the population.     (Parameter)   10% of US Senators voted for a particular measure. There are only 100 US Senators, you can count how every single one of them voted.     (Parameter)   40% of 1,211 students at a particular elementary school got below a 3 on a standardized test. You know this because you have each and every student's test score.    "
+  "body": " Statistics Terminology   This section corresponds to Section 1.2 in Dietz    Population  Population   A population is a collection of individuals (not necessarily people) that share one or more common characteristics. In statistics this is the entire group of individuals for which information is being gathered or studied. Information that describes the population is called descriptive statistics.     Individual  Individual   An individual is a member of a particular population     Sample  Sample   A sample is a subset of the population. In statistics, it is often not feasible or desirable to gather information about the whole population so information is gathered on part of the population being studied. Inferential Statistics is when the information gathered about the sample is used to say something about the entire population.     Variable  Variable  Variable (Quantitative)  Variable (Qualitative)  Variable (Categorical)   A variable is a quantity or quality of the population that varies among individuals. A variable can be either quantitative or qualitative (categorical). A quantitative variable can be measured and can often be used in the calculation of an average. Quantitative variables have numerical values. Categorical variables are often a verbal description of the population but they can sometimes be numerical if they are used to categorize individuals in the population. Examples of this are zip codes, phone numbers, and instances when numbers are substituted for a particular quality such as letting 0 represent Males and 1 Females.     Parameter  Parameter   A parameter is a value found by looking at the entire population. Information you see about small populations is often parameters because it is not too hard to gather information about each individual in the population.     (Parameter)   10% of US Senators voted for a particular measure. There are only 100 US Senators, you can count how every single one of them voted.     (Parameter)   40% of 1,211 students at a particular elementary school got below a 3 on a standardized test. You know this because you have each and every student's test score.     Statistic  Statistic   A statistic is a value found from looking at a sample of a population.    In actuality, many times you are not told that the value comes from a sample but you may come to this conclusion if it seems very unlikely that each individual in the population got measured\/polled. When you see information about a large population it is very possible the data that you see is a statistic and not a parameter.   (Statistic)   88% of Oklahomans would support a federal law requiring background checks for all gun purchases, It’s not possible to actually ask millions of people whether they agree. Researchers instead take a poll using a sample of Oklahomans.     (Statistic)   40% of Oklahoma City residents report that they have been to at least one Thunder game. It’s very doubtful that anyone polled each resident in Oklahoma City. They took a sample, so they have a statistic.     (Statistic)   30% of dog owners poop scoop after their dog. It’s impossible to survey all dog owners—no one keeps an accurate track of exactly how many people own dogs. This data had to be from a sample, so it’s a statistic.    "
 },
 {
   "id": "remark-1",
@@ -61,7 +61,7 @@ var ptx_lunr_docs = [
   "type": "Definition",
   "number": "1.1.5",
   "title": "Variable.",
-  "body": " Variable  Variable   A variable is a quantity or quality of the population that varies among individuals. A variable can be either quantitative or qualitative (categorical). A quantitative variable can be measured and can often be used in the calculation of an average. Quantitative variables have numerical values. Categorical variables are often a verbal description of the population but they can sometimes be numerical if they are used to categorize individuals in the population. Examples of this are zip codes, phone numbers, and instances when numbers are substituted for a particular quality such as letting 0 represent Males and 1 Females.   "
+  "body": " Variable  Variable  Variable (Quantitative)  Variable (Qualitative)  Variable (Categorical)   A variable is a quantity or quality of the population that varies among individuals. A variable can be either quantitative or qualitative (categorical). A quantitative variable can be measured and can often be used in the calculation of an average. Quantitative variables have numerical values. Categorical variables are often a verbal description of the population but they can sometimes be numerical if they are used to categorize individuals in the population. Examples of this are zip codes, phone numbers, and instances when numbers are substituted for a particular quality such as letting 0 represent Males and 1 Females.   "
 },
 {
   "id": "def-parameter",
@@ -89,6 +89,51 @@ var ptx_lunr_docs = [
   "number": "1.1.8",
   "title": "(Parameter).",
   "body": " (Parameter)   40% of 1,211 students at a particular elementary school got below a 3 on a standardized test. You know this because you have each and every student's test score.   "
+},
+{
+  "id": "def-statistic",
+  "level": "2",
+  "url": "statistics-terminology.html#def-statistic",
+  "type": "Definition",
+  "number": "1.1.9",
+  "title": "Statistic.",
+  "body": " Statistic  Statistic   A statistic is a value found from looking at a sample of a population.   "
+},
+{
+  "id": "example-3",
+  "level": "2",
+  "url": "statistics-terminology.html#example-3",
+  "type": "Example",
+  "number": "1.1.10",
+  "title": "(Statistic).",
+  "body": " (Statistic)   88% of Oklahomans would support a federal law requiring background checks for all gun purchases, It’s not possible to actually ask millions of people whether they agree. Researchers instead take a poll using a sample of Oklahomans.   "
+},
+{
+  "id": "example-4",
+  "level": "2",
+  "url": "statistics-terminology.html#example-4",
+  "type": "Example",
+  "number": "1.1.11",
+  "title": "(Statistic).",
+  "body": " (Statistic)   40% of Oklahoma City residents report that they have been to at least one Thunder game. It’s very doubtful that anyone polled each resident in Oklahoma City. They took a sample, so they have a statistic.   "
+},
+{
+  "id": "example-5",
+  "level": "2",
+  "url": "statistics-terminology.html#example-5",
+  "type": "Example",
+  "number": "1.1.12",
+  "title": "(Statistic).",
+  "body": " (Statistic)   30% of dog owners poop scoop after their dog. It’s impossible to survey all dog owners—no one keeps an accurate track of exactly how many people own dogs. This data had to be from a sample, so it’s a statistic.   "
+},
+{
+  "id": "types-of-variables",
+  "level": "1",
+  "url": "types-of-variables.html",
+  "type": "Section",
+  "number": "1.2",
+  "title": "Types of Variables",
+  "body": " Types of Variables  "
 }
 ]
 
